4e753026c54144bd17e78ff75ae15375ef03e010
Rahim10020 <rahialighi@gmail.com>
2026-09-15 14:47:45 +0000

    update app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { EstablishmentsModule } from './modules/establishments/establishments.module.js';
+import { AuthModule } from './modules/auth/auth.module.js';
 
 @Module({
-  imports: [PrismaModule, EstablishmentsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    EstablishmentsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
