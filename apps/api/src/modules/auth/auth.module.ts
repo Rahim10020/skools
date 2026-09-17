51c77abdac79df6b1f8b4771293cad186f9b99fc
Rahim10020 <rahialighi@gmail.com>
2026-09-17 20:12:09 +0000

    fix errors

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -22,6 +22,6 @@ import { JwtStrategy } from './strategies/jwt.strategy.js';
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
-  exports: [AuthService],
+  exports: [AuthService, PassportModule],
 })
 export class AuthModule {}
--- apps/api/src/modules/establishments/establishments.module.ts
+++ apps/api/src/modules/establishments/establishments.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module.js';
 import { EstablishmentsService } from './establishments.service.js';
 import { EstablishmentsController } from './establishments.controller.js';
 
 @Module({
+  imports: [AuthModule],
   controllers: [EstablishmentsController],
   providers: [EstablishmentsService],
   exports: [EstablishmentsService],
