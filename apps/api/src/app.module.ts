7a7087b80e9f9e7ed5a33cf7d96149d88e991cec
Rahim10020 <rahialighi@gmail.com>
2026-09-17 20:35:38 +0000

    add academic module to app

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { EstablishmentsModule } from './modules/establishments/establishments.module.js';
 import { AuthModule } from './modules/auth/auth.module.js';
+import { AcademicYearsModule } from './modules/academic/academic-years/academic-years.module.js';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AuthModule } from './modules/auth/auth.module.js';
     }),
     PrismaModule,
     EstablishmentsModule,
+    AcademicYearsModule,
     AuthModule,
   ],
   controllers: [AppController],
