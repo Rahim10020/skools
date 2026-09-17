06408fc190c16329e7169c5c8da629469161715a
Rahim10020 <rahialighi@gmail.com>
2026-09-17 20:40:58 +0000

    fix error

--- apps/api/src/modules/academic/academic-years/academic-years.module.ts
+++ apps/api/src/modules/academic/academic-years/academic-years.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../../auth/auth.module.js';
 import { AcademicYearsService } from './academic-years.service.js';
 import { AcademicYearsController } from './academic-years.controller.js';
 
 @Module({
+  imports: [AuthModule],
   controllers: [AcademicYearsController],
   providers: [AcademicYearsService],
   exports: [AcademicYearsService],
