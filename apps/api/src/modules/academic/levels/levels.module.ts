204cb91037ee743594e741bdb569b0cf40e116a1
Rahim10020 <rahialighi@gmail.com>
2026-09-18 14:08:43 +0000

    fix bugs

--- apps/api/src/modules/academic/levels/levels.module.ts
+++ apps/api/src/modules/academic/levels/levels.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LevelsService } from './levels.service.js';
 import { LevelsController } from './levels.controller.js';
+import { AuthModule } from '../../auth/auth.module.js';
 
 @Module({
+  imports: [AuthModule],
   controllers: [LevelsController],
   providers: [LevelsService],
   exports: [LevelsService],
