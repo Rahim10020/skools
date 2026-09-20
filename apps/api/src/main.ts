7698c8111419db9d894ef47dc1a59a8e29cd95cd
Rahim10020 <rahialighi@gmail.com>
2026-09-20 13:50:37 +0000

    enable cors origin in api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Activer CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // URL du frontend
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
