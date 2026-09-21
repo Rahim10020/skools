295a8bf78a3232b3bf9711001f1c5c01fa2db03e
Rahim10020 <rahialighi@gmail.com>
2026-09-21 21:11:47 +0000

    coding dtos

---
+++ apps/api/src/modules/parents/dto/create-parent.dto.ts
@@ -0,0 +1,35 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateParentDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  lastName: string;
+
+  @IsString()
+  @IsOptional()
+  phone?: string;
+
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  @IsOptional()
+  address?: string;
+
+  @IsString()
+  @IsOptional()
+  profession?: string;
+}
